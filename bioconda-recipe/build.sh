a5318d95f30ae394fd4ff4df7758e3737c66db79
Claudio Lottaz <claudio.lottaz@ur.de>
2019-02-22 22:36:53 +0100

    fix build.sh for new C compiler Changes to be committed: 	modified:   build.sh

--- bioconda-recipe/build.sh
+++ bioconda-recipe/build.sh
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+Rscript -e 'install.packages(c("rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
+cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=$PREFIX
+=======
 #!/bin/bash
 
 Rscript -e 'install.packages(c("rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
-cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_COMPILER=${CC}
+>>>>>>> d05c95943a9e662feb65054dc303a1df3e7e11fe
 cd build
 make
 make install
