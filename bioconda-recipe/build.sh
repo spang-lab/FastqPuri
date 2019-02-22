d1754538ab42052bf70640328912c82c971ee759
Claudio Lottaz <claudio.lottaz@ur.de>
2019-02-22 20:03:39 +0100

    fixed issues of files used in the bioconda-recipe git repository Changes to be committed: 	modified:   bioconda-recipe/build.sh 	modified:   bioconda-recipe/meta.yaml

--- bioconda-recipe/build.sh
+++ bioconda-recipe/build.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+
 Rscript -e 'install.packages(c("rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
-echo Prefix $PREFIX
-cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${PREFIX}
 cd build
 make
 make install
