28299705f2e1efa321647eb2fbea984ad4e74941
Claudio Lottaz <claudio.lottaz@ur.de>
2019-02-23 20:27:27 +0100

    fixed bioconda build.sh added output for debug issue in Rcompdiag_Qreport.c and config.h.in Changes to be committed: 	modified:   bioconda-recipe/build.sh 	modified:   config.h.in 	modified:   src/Rcommand_Qreport.c

--- bioconda-recipe/build.sh
+++ bioconda-recipe/build.sh
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-Rscript -e 'install.packages(c("rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
-cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=$PREFIX
-=======
 #!/bin/bash
 
-Rscript -e 'install.packages(c("rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
+Rscript -e 'install.packages(c("digest", "htmltools", "rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
 cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_COMPILER=${CC}
->>>>>>> d05c95943a9e662feb65054dc303a1df3e7e11fe
 cd build
 make
 make install
